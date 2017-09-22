34a1d5952f2136168f8bc0ea648bfdfa025b23d0
Ryan Lucas <rlucas@elliemae.com>
2017-09-22 12:09:34 -0700

    idempotency for the win

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # mac-setup
 
+## Usage: 
+> ./install.sh
+
 To use the Brewfile, tap homebrew/bundle (one time) to install the command, then run it in a directory with a Brewfile in it:
 
 > brew tap homebrew/bundle &&
--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-echo "Installing Homebrew"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-echo "Installing bundle"
-brew tap Homebrew/bundle
+which brew > /dev/null
+if [ $? !eq 0 ]; then
+	echo "Installing Homebrew"
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "brew already installed"
+fi
+brew help bundle > /dev/null
+if [ $? -eq 0 ]; then
+	echo "Installing bundle"
+	brew tap Homebrew/bundle
+fi
 echo "Installing Brewfile dependencies"
 brew bundle
