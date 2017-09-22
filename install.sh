948ed7608003ef6dfc789cc047337253dbacbeb5
Justin Brodley <justin.brodley@elliemae.com>
2017-09-22 10:47:40 -0700

    Added bundle install

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-echo "Installing Brewfile"
+echo "Installing bundle"
+brew tap Homebrew/bundle
+echo "Installing Brewfile dependencies"
 brew bundle
