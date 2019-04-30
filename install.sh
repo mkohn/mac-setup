45c6775f9394428071685710187d7cc5107b04fb
Justin Brodley <jbrodley@gmail.com>
2019-04-30 12:03:20 -0700

    Added .bash profile code changes
    Adding in several commands i use often in bash profile.  This includes brewup, usecreds, listcreds and cflint.  This was missing and caught me by suprise in my last new computer update. 
    I should move this to a file (bash_profilte.txt) and than move to tee.  Example code for future enhancement. 
    
    tee -a file.txt <<EOF
    
    I am "Finding" difficult to write this to file
    I can "write" without double quotes
    EOF

--- install.sh
+++ install.sh
@@ -5,3 +5,22 @@ echo "Installing bundle"
 brew tap Homebrew/bundle
 echo "Installing Brewfile dependencies"
 brew bundle
+
+echo "cflint() {
+   #Run AWS Cloudformation Validate with Filename and Profile
+   aws cloudformation validate-template --template-body file://./$1 --profile=$2
+}
+
+#-> listcreds [] list declared AWS credentials
+function listcreds {
+        cat  ~/.aws/credentials |  awk -F '[][]' '{print $2}' | sed '/^\s*$/d'
+}
+#-> usecreds [] use declared AWS creds
+function usecreds {
+        export AWS_PROFILE="$1"
+}
+
+# Update HomeBrew and Casks
+brewup() {
+	brew update && brew upgrade && brew cu -a && brew cleanup
+}" >> ~/.bash_profile
