b89e4909baf223bd37ac72c7fb4f65c2921bc586
Matthew Kohn <mkohn@users.noreply.github.com>
2020-05-22 09:45:54 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,29 +39,4 @@ git config --global init.templateDir ~/.git-templates/git-secrets
 # Copy SSH config
 cp ./ssh-config ~/.ssh/config
 
-## Needs to be run as root user section
-sudo -s <<EOS
-#Disable Zscaler
-grep "zscalertwo" /etc/hosts > /dev/null
-ZHOSTSFILE=$?
-if [[ $ZHOSTSFILE -eq 0 ]]; then
-	echo "Zscaler entries already present"
-else 
-	echo "Adding needed entries to hostsfile"
-	cat <<EOT >> /etc/hosts
-127.0.0.1 pac.zscalertwo.net
-127.0.0.1 gateway.zscalertwo.net
-EOT
-fi
-
-grep '104.129.192.0/20' /etc/pf.conf > /dev/null
-ZRULES=$?
-if [[  $ZRULES -eq 0 ]]; then
-	echo "Zscaler entry already present"
-else
-	echo "Blocking proxy traffic"
-	echo "block drop from any to 104.129.192.0/20" >> /etc/pf.comf
-	pfctl -f /etc/pf.conf 2> /dev/null
-	pfctl -E 2> /dev/null
-fi	
-EOS
+/usr/local/opt/fzf/install
